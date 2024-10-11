b1bf8c2799df7adf809971cab1f6aa55c8ac0fce
Kathy Bauer <kbauer17@my.wctc.edu>
2024-10-11 12:39:27 -0500

    AddData:  add validation for required fields

--- Models/DemoJobData.cs
+++ Models/DemoJobData.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using Google.Cloud.Firestore;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,21 +10,27 @@ public class DemoJobData
     public DemoJobData(){}
 
     [FirestoreProperty]
+    [Required(ErrorMessage = "Please enter your initials")]
     public required string Initials {get; set;}
 
     [FirestoreProperty]
+    [Required(ErrorMessage = "Please enter a Job Number")]
     public required string JobNum {get; set;}
     
     [FirestoreProperty]
+    [Required(ErrorMessage = "Please select a Start Time")]
     public required string StartTime {get; set;}
 
     [FirestoreProperty]
+    [Required(ErrorMessage = "Please select an End Time")]
     public required string EndTime {get; set;}
 
     [FirestoreProperty]
+    [Required(ErrorMessage = "Please enter the Quantity")]
     public required int Quantity{get; set;}
 
     [FirestoreProperty]
+    [Required(ErrorMessage = "Please select a Date")]
     public required DateTime DateToday {get; set;}= DateTime.Now; // default to today's date
 
     [FirestoreProperty]
@@ -31,6 +38,7 @@ public class DemoJobData
 
     [BindProperty]
     [FirestoreProperty]
+    [Required(ErrorMessage = "Please select an Operation")]
     public required int JobOpId {get; set;}
 
     public required virtual JobOp JobOp {get; set;}

01a3c845978cbabfb5b56196d202427dd3892440
Kathy Bauer <kbauer17@my.wctc.edu>
2024-10-15 08:34:37 -0500

    'EditData' HttpPost completed

--- Controllers/FirestoreController.cs
+++ Controllers/FirestoreController.cs
@@ -7,9 +7,11 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 public class FirestoreController : Controller
 {
     private readonly FirestoreDb _firestoreDb;
+    private readonly ILogger<FirestoreController> _logger;
 
-    public FirestoreController(FirestoreDb firestoreDb){
+    public FirestoreController(FirestoreDb firestoreDb, ILogger<FirestoreController> logger){
         _firestoreDb = firestoreDb;
+        _logger = logger;
     }
 
     /// <summary>
@@ -25,7 +27,8 @@ public class FirestoreController : Controller
         QuerySnapshot snapshot2 = await collection2.GetSnapshotAsync();
         var operations = snapshot2.Documents.Select(d => d.ConvertTo<JobOp>()).ToList();
 
-        var result = from d in documents join o in operations on d.JobOpId equals o.Id
+        var result = from d in documents 
+                        join o in operations on d.JobOpId equals o.Id
                         select new JobDataViewModel
                             {
                                 DemoJobData = d,
@@ -45,7 +48,6 @@ public class FirestoreController : Controller
         QuerySnapshot snapshot = await collection.GetSnapshotAsync();
         var operations = snapshot.Documents.Select(d => d.ConvertTo<JobOp>()).ToList();
         ViewData["JobOps"] = new SelectList(operations, "Id", "OpName");
-        
 
         return View();
     }
@@ -96,6 +98,7 @@ public class FirestoreController : Controller
         CollectionReference collection2 = _firestoreDb.Collection("JobOp");
         QuerySnapshot snapshot2 = await collection2.GetSnapshotAsync();
         var operations = snapshot2.Documents.Select(d => d.ConvertTo<JobOp>()).ToList();
+       ViewData["JobOps"] = new SelectList(operations, "Id", "OpName");
 
         var result = from d in documents 
                         join o in operations on d.JobOpId equals o.Id
@@ -109,6 +112,52 @@ public class FirestoreController : Controller
         var specificDocumentModel = result.FirstOrDefault();
 
         return View(specificDocumentModel);   
+    }
+
+    /// <summary>
+    /// Edit a document in the database collection named "DemoJobData"
+    /// </summary>
+    /// <param name="demoJobData"></param>
+    /// <returns></returns>
+    [HttpPost]
+    public async Task<IActionResult>EditData(JobDataViewModel model)
+    {
+        var jobDataId = model.DemoJobData.JobDataId ?? Guid.NewGuid().ToString(); // coalesce expression checking for null
+        DateTime utcDate = model.DemoJobData.DateToday.Date.ToUniversalTime();
+
+        DateTime dateTime1 = DateTime.ParseExact(model.DemoJobData.StartTime, "HH:mm", null);
+        DateTime dateTime2 = DateTime.ParseExact(model.DemoJobData.EndTime, "HH:mm", null);
+        TimeSpan difference = dateTime2 - dateTime1;
+        
+        var collection = _firestoreDb.Collection("DemoJobData");
+        var query = collection.WhereEqualTo("JobDataId", model.DemoJobData.JobDataId);
+        var snapshot = await query.GetSnapshotAsync();
+
+        if(!snapshot.Documents.Any())
+        {
+            _logger.LogError("Document not found with JobDataID: {JobDataId}", model.DemoJobData.JobDataId);
+            return NotFound();
+        }
+
+        var documentReference = snapshot.Documents[0].Reference;
+
+
+        var updateData = new Dictionary<string, object>
+        {
+            {"JobDataId", jobDataId},
+            {"Initials", model.DemoJobData.Initials},
+            {"JobNum", model.DemoJobData.JobNum},
+            {"StartTime", model.DemoJobData.StartTime},
+            {"EndTime", model.DemoJobData.EndTime},
+            {"Quantity", model.DemoJobData.Quantity},
+            {"DateToday", utcDate},
+            {"Minutes", difference.TotalMinutes},
+            {"JobOpId", model.JobOp.Id}
+        };
+
+        await documentReference.SetAsync(updateData);
+
+        return RedirectToAction("ViewAllData");
     }
 
 }
